fb11a3a14dd5a58a6d418d91b653e17ebc0df270
nicoantoniw <nicolasantoniw@gmail.com>
2022-09-09 18:23:31 +0200

    updated cors

--- app.js
+++ app.js
@@ -48,7 +48,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.use(cors());
+app.use(cors({
+  origin: "https://www.nicolasantoniw.me",
+  credentials: true,
+}));
 
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', 'https://www.nicolasantoniw.me');
