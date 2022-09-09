c62cac3a8b666b4b7d824b7d87fe4b37a67ab294
nicoantoniw <nicolasantoniw@gmail.com>
2022-09-09 17:36:41 +0200

    updated cors

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://nicolasantoniw.me');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.nicolasantoniw.me');
   res.setHeader(
     'Access-Control-Allow-Methods',
     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
